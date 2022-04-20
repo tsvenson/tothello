fd370d2e00200422143f6a26b3dc1d0924216209
Thomas Svensson <thomas@tsvenson.com>
2022-04-20 13:09:41 +0200

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,15 @@
+Tothello - Terminal Othello
+===========================
+
+Tothello is a code learning project using:
+
+- Python 3.10.x
+- `Textual <https://github.com/Textualize/textual>`_ - Uses Textual CSS, so currently needs dev version
+- `Rich <https://github.com/Textualize/rich>`_
+
+Developed on Ubuntu 20.04.3 LTS, with no attempts making it work on other OS's.
+
+The goal is to build an **Othello**-clone that runs in the Bash shell on Linux.
+It will have an extremely simple game AI, so one player actually can play against
+the computer.
+
---
+++ src/tothello/tothello.py
@@ -0,0 +1,24 @@
+"""
+Othello game written in Python using Textual as TUI in Bash for Linux.
+"""
+# Setup game
+# Ask player name
+# Ask white or black
+# Initial board
+# Check if player can make a move
+# Take player input for placing piece
+# Check if move is valid input
+# Place piece and flip opponents pieces affected
+# Update score
+# Keep track on remaining possible turns
+# Skip players turn if no move is possible
+# Declare winner when all pieces are placed or no more moves possible
+# Play again?
+
+
+def main():
+    print('Lets play Tothello')
+
+
+if __name__ == "__main__":
+    main()
